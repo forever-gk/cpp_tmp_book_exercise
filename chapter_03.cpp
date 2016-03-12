74dab1a7bf60a2e755db521d4f92084c4f1d32b9
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-03-13 07:00:17 +0900

    added solution for 3-5.

--- chapter_03.cpp
+++ chapter_03.cpp
@@ -143,6 +143,13 @@ namespace dimension_analysis
                 : m_value(x)
         { }
 
+        template <typename OtherDimensions>
+        quantity(quantity<T, OtherDimensions> const& q)
+                : m_value(q.value())
+        {
+            static_assert(mpl::equal<Dimensions, OtherDimensions>(), "OtherDimensions should be same as Dimensions.");
+        }
+
     public:
         T value() const
         {
@@ -152,6 +159,50 @@ namespace dimension_analysis
     private:
         T m_value;
     };
+
+    template <typename T1, typename T2, typename D1, typename D2>
+    auto operator + (quantity<T1, D1> const& lhs, quantity<T2, D2> const& rhs)
+    {
+        static_assert(mpl::equal<D1, D2>(), "D1 and D2 dimensions should be same.");
+        return quantity<decltype(lhs.value() + rhs.value()), D1>(lhs.value() + rhs.value());
+    }
+
+    template <typename T1, typename T2, typename D1, typename D2>
+    auto operator - (quantity<T1, D1> const& lhs, quantity<T2, D2> const& rhs)
+    {
+        static_assert(mpl::equal<D1, D2>(), "D1 and D2 dimensions should be same.");
+        return quantity<decltype(lhs.value() + rhs.value()), D1>(lhs.value() - rhs.value());
+    }
+
+    using mpl::placeholders::_;
+
+    template <typename D1, typename D2>
+    struct multiply_dimensions
+            : mpl::transform<D1, D2, mpl::plus<_, _>>
+    { };
+
+    template <typename T1, typename T2, typename D1, typename D2>
+    auto operator * (quantity<T1, D1> const& lhs, quantity<T2, D2> const& rhs)
+    {
+        return quantity<
+                   decltype(lhs.value() * rhs.value()),
+                   typename multiply_dimensions<D1, D2>::type
+               >(lhs.value() * rhs.value());
+    }
+
+    template <typename D1, typename D2>
+    struct divide_dimensions
+            : mpl::transform<D1, D2, mpl::minus<_, _>>
+    { };
+
+    template <typename T1, typename T2, typename D1, typename D2>
+    auto operator / (quantity<T1, D1> const& lhs, quantity<T2, D2> const& rhs)
+    {
+        return quantity<
+                decltype(lhs.value() / rhs.value()),
+                typename divide_dimensions<D1, D2>::type
+               >(lhs.value() / rhs.value());
+    }
 } // namespace dimension_analysis
 
 
@@ -159,5 +210,31 @@ TEST_CASE("3-5", "[tmp]")
 {
     using namespace dimension_analysis;
 
+    quantity<float, length> l{ 1.0f };
+    quantity<float, mass> m{ 2.0f };
+
+    // expected compile error
+    //m = l;
+
+    quantity<float, length> len1{ 1.0f };
+    quantity<float, length> len2{ 2.0f };
+
+    len1 = len1 + len2;
+    REQUIRE(len1.value() == 3.0f);
+
+    // expected compile error
+    //len1 = len2 + quantity<float, mass>{ 3.7f };
+
+    quantity<float, mass> m1{ 5.0f };
+    quantity<float, acceleration> a1{ 9.8f };
+    REQUIRE((m1 * a1).value() == 49.0f);
+
+    quantity<float, force> f1 = m1 * a1;
+
+    quantity<float, mass> m2 = f1 / a1;
+    float rounding_error = std::abs((m2 - m1).value());
+
+    f1 = f1 + m1 * a1;
+    REQUIRE(f1.value() == 98.0f);
 }
 
