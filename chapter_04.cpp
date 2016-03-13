9904cd649c354486202f0cca2f1f899c82a951f1
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-03-14 02:28:35 +0900

    did some typing for the rest exercises.

--- chapter_04.cpp
+++ chapter_04.cpp
@@ -1,9 +1,19 @@
 #include "catch.hpp"
 
 #include <type_traits>
+#include <algorithm>
 
 #include <boost/mpl/or.hpp>
 #include <boost/mpl/and.hpp>
+#include <boost/mpl/if.hpp>
+#include <boost/mpl/apply.hpp>
+#include <boost/mpl/next.hpp>
+#include <boost/mpl/not_equal_to.hpp>
+#include <boost/mpl/greater.hpp>
+#include <boost/mpl/minus.hpp>
+#include <boost/mpl/plus.hpp>
+#include <boost/mpl/multiplies.hpp>
+#include <boost/mpl/int.hpp>
 
 namespace mpl = boost::mpl;
 
@@ -102,5 +112,72 @@ TEST_CASE("4-2", "[tmp]")
             >(),
             ""
     );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+template <typename N, typename Predicate>
+struct next_if
+        : mpl::if_<
+                typename mpl::apply<Predicate, N>::type,
+                typename mpl::next<N>::type,
+                N
+            >
+{ };
+
+template <typename N1, typename N2>
+struct formula
+        : mpl::if_<
+                mpl::not_equal_to<N1, N2>,
+                typename mpl::if_<
+                                mpl::greater<N1, N2>,
+                                typename mpl::minus<N1, N2>::type,
+                                N1
+                            >::type,
+                typename mpl::plus<
+                                N1,
+                                typename mpl::multiplies<N1, mpl::int_<2>>
+                            >::type
+            >::type
+{ };
+
+
+TEST_CASE("4-3", "[tmp]")
+{
+
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+template <typename T>
+struct is_data_member_pointer;
+
+template <typename T>
+struct is_pointer_to_function;
+
+template <typename T>
+struct is_reference_to_function_pointer;
+
+template <typename T>
+struct is_reference_to_non_const;
+
+
+TEST_CASE("4-4", "[tmp]")
+{
+
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+template <typename Container, typename Value>
+typename Container::iterator
+container_find(Container & c, Value const& v)
+{
+    return std::find(c.begin(), c.end(), v);
+}
+
+
+TEST_CASE("4-5", "[tmp]")
+{
+
 }
 
