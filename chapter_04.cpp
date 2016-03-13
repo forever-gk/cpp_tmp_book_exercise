442913de07154252facbf8392a537630fc242550
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-03-14 02:07:41 +0900

    added solution for 4-1 and 4-2.

--- chapter_04.cpp
+++ chapter_04.cpp
@@ -19,4 +19,88 @@ TEST_CASE("4-0", "[tmp]")
     static_assert(!mpl::and_<std::is_pointer<type_t>, std::is_integral<type_t>>(), "");
 }
 
+////////////////////////////////////////////////////////////////////////////////
+template <typename... Condition>
+struct logical_or;
+
+template <>
+struct logical_or<> : std::false_type
+{ };
+
+template <typename x, typename... xs>
+struct logical_or<x, xs...>
+        : std::conditional_t<
+                x::value,
+                std::true_type,
+                logical_or<xs...>
+            >
+{ };
+
+
+template <typename... Condition>
+struct logical_and;
+
+template <>
+struct logical_and<> : std::true_type
+{ };
+
+template <typename x, typename... xs>
+struct logical_and<x, xs...>
+        : std::conditional_t<
+                x::value,
+                logical_and<xs...>,
+                std::false_type
+            >
+{ };
+
+
+TEST_CASE("4-1", "[tmp]")
+{
+    using type_t = int;
+
+    static_assert(logical_or<std::is_integral<type_t>, std::is_pointer<type_t>>(), "");
+    static_assert(logical_or<std::is_pointer<type_t>, std::is_integral<type_t>>(), "");
+
+    static_assert(!logical_and<std::is_integral<type_t>, std::is_pointer<type_t>>(), "");
+    static_assert(!logical_and<std::is_pointer<type_t>, std::is_integral<type_t>>(), "");
+}
+
+TEST_CASE("4-2", "[tmp]")
+{
+    using type_t = int;
+
+    static_assert(
+            logical_or<
+                    std::is_integral<type_t>,
+                    std::is_pointer<type_t>,
+                    std::is_function<type_t>
+            >(),
+            ""
+    );
+    static_assert(
+            logical_or<
+                    std::is_pointer<type_t>,
+                    std::is_integral<type_t>,
+                    std::is_function<type_t>
+            >(),
+            ""
+    );
+
+    static_assert(
+            !logical_and<
+                    std::is_integral<type_t>,
+                    std::is_pointer<type_t>,
+                    std::is_function<type_t>
+            >(),
+            ""
+    );
+    static_assert(
+            !logical_and<
+                    std::is_pointer<type_t>,
+                    std::is_integral<type_t>,
+                    std::is_function<type_t>
+            >(),
+            ""
+    );
+}
 
