7636e3a236c8973a6ec9bbc573e9f62709fbd43b
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-03-14 01:46:32 +0900

    added solution for 4-0.

--- chapter_04.cpp
+++ chapter_04.cpp
@@ -1,8 +1,22 @@
 #include "catch.hpp"
 
+#include <type_traits>
+
+#include <boost/mpl/or.hpp>
+#include <boost/mpl/and.hpp>
+
+namespace mpl = boost::mpl;
+
 
 TEST_CASE("4-0", "[tmp]")
 {
+    using type_t = int;
 
+    static_assert(mpl::or_<std::is_integral<type_t>, std::is_pointer<type_t>>(), "");
+    static_assert(mpl::or_<std::is_pointer<type_t>, std::is_integral<type_t>>(), "");
+
+    static_assert(!mpl::and_<std::is_integral<type_t>, std::is_pointer<type_t>>(), "");
+    static_assert(!mpl::and_<std::is_pointer<type_t>, std::is_integral<type_t>>(), "");
 }
+
 
