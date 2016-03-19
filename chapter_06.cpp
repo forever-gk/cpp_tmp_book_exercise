8ea72699aee4df23a3323df1302ad56f86295159
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-03-20 00:03:32 +0900

    added multi-character sequence test case.

--- chapter_06.cpp
+++ chapter_06.cpp
@@ -5,6 +5,7 @@
 #include <boost/mpl/if.hpp>
 #include <boost/mpl/less_equal.hpp>
 #include <boost/mpl/vector.hpp>
+#include <boost/mpl/string.hpp>
 #include <boost/mpl/copy.hpp>
 #include <boost/mpl/fold.hpp>
 #include <boost/mpl/front.hpp>
@@ -64,5 +65,37 @@ TEST_CASE("6-0", "[tmp]")
             >(),
             ""
     );
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+TEST_CASE("multi-character sequence", "[tmp]")
+{
+    auto cs = 'abcd';
+    static_assert(std::is_same<int, decltype(cs)>(), "");
+    static_assert(4 == sizeof(cs), "");
+    REQUIRE('d' == ((cs >> 0)  & 0xFF));
+    REQUIRE('c' == ((cs >> 8)  & 0xFF));
+    REQUIRE('b' == ((cs >> 16) & 0xFF));
+    REQUIRE('a' == ((cs >> 24) & 0xFF));
+
+    /*
+    // NOTE: compile warning on clang: character constant too long for its type
+    auto cs1 = 'abcde';
+    static_assert(std::is_same<int, decltype(cs1)>(), "");
+    static_assert(4 == sizeof(cs1), "");
+    REQUIRE('e' == ((cs1 >> 0)  & 0xFF));
+    REQUIRE('d' == ((cs1 >> 8)  & 0xFF));
+    REQUIRE('c' == ((cs1 >> 16) & 0xFF));
+    REQUIRE('b' == ((cs1 >> 24) & 0xFF));
+
+    // NOTE: 'a' was truncated.
+    //       compile warning on clang: shift count >= width of type
+    REQUIRE('a' != ((cs1 >> 32) & 0xFF));
+     */
+}
+
+TEST_CASE("6-1", "[tmp]")
+{
 }
 
